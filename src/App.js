73b26e5e804fc262fc14809747a4382014945bcf
borisdavid77 <borisdavid779@gmail.com>
2022-01-26 23:42:53 -0500

    second changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, My Name is
-        </p>
-        <h2>Boris Zambrano Suarez</h2>
+      <header class="header" id="header">
+        <nav class="nav container">
+          <a href="" class="nav_logo">Boris Zambrano</a>
+          <div class="nav_menu" id="nav-menu">
+            <ul class="nav_list grid">
+              <li class="nav_item">
+                <a class="nav_link">
+                  <i class="uil uil-user nav_icon"></i>About me
+                </a>
+              </li>
+              <li class="nav_item">
+                <a class="nav_link">
+                  <i class="uil uil-file-plus-alt nav_icon"></i>Works
+                </a>
+              </li>
+              <li class="nav_item">
+                <a class="nav_link">
+                  <i class="uil uil-message nav_icon"></i>Contact
+                </a>
+              </li>
+            </ul>
+            <i class="uil uil-times nav_close" id="nav-close"></i>
+          </div>
+
+          <div class="nav_btns">
+
+
+            <i class="uil uil-moon change-theme nav_toggle" id="theme-button"></i>
+
+            <div class="nav_toggle" id="nav-toggle">
+              <i class="uil uil-apps"></i>
+            </div>
+          </div>
+        </nav>
       </header>
     </div>
   );
