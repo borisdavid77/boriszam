9b9b20030997c018a34eefcd0085047d9bf09822
borisdavid77 <borisdavid779@gmail.com>
2022-01-26 22:48:27 -0500

    first deploy react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, My Name is
+        </p>
+        <h2>Boris Zambrano Suarez</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
